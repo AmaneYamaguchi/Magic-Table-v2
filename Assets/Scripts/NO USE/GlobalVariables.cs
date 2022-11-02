72a5d65a6203a7642694ff098f3ae0d40982cff8
AmaneYamaguchi <yamabach@icloud.com>
2022-11-02 14:03:06 +0900

    commented out GlobalVariables

---
+++ Assets/Scripts/NO USE/GlobalVariables.cs
@@ -0,0 +1,21 @@
+﻿/// <summary>
+/// なるべくシングルトンでどうにかしたいやつ
+/// </summary>
+//public static class GlobalVariables
+//{
+    /// <summary>
+    /// テーブルの形状
+    /// </summary>
+    /*
+    public enum TableShape
+    {
+        triangle = 3,
+        square = 4,
+        pentagon = 5,
+    };
+    */
+
+    //public static TableShape Shape = TableShape.square;// シーン番号
+	//public static float Gain = 4f/3f;
+
+//}
