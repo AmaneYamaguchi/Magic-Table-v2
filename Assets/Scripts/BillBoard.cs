962f63270c676c632176b7f9034da545de76ac33
AmaneYamaguchi <yamabach@icloud.com>
2022-11-14 18:44:22 +0900

    build apk to 5th Quest Pro

---
+++ Assets/Scripts/BillBoard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 常にカメラの方向を向くオブジェクト
+/// </summary>
+public class BillBoard : MonoBehaviour
+{
+    /// <summary>
+    /// ターゲットにするオブジェクト（カメラ想定）
+    /// </summary>
+    public Transform Target;
+    /// <summary>
+    /// y軸を固定するかどうか
+    /// </summary>
+    public bool YConstraint = true;
+
+    void Update()
+    {
+        var targetPos = YConstraint ? new Vector3(Target.position.x, transform.position.y, Target.position.z) : Target.position;
+        transform.LookAt(targetPos, Vector3.up);
+    }
+}
